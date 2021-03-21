c6afa0f7b157edca4473483985f9c69dfc70befb
nsum <your.marketer1303@gmail.com>
2021-03-21 03:55:00 +0000

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,26 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
