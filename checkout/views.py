5394727907d54cd18f8d513db22e382a9204bb6c
nsum <your.marketer1303@gmail.com>
2021-04-03 02:15:30 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IbyReGeo3hIAosYz67NiaOYbwC7NxfyKN8dmKutXoyU0X3Gk9eTDlUwyaFAuhtcImVvIa96wwXNYALJOZQqn7H600I8esLO1K',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
