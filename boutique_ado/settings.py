5a4f1a3410d8e665997a7fe810afa8621c6b7780
nsum <your.marketer1303@gmail.com>
2021-04-09 01:39:58 +0000

    Add static control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -169,6 +169,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     AWS_STORAGE_BUCKET_NAME = 'nsum-shop'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
